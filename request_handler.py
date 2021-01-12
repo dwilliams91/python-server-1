ddfbd33e98dcbff7092766e80be306a4f78c3022
Your Github User Name <david.lav.williams@gmail.com>
2021-01-12 13:36:11 -0600

    working on put

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 import json
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -100,26 +100,41 @@ class HandleRequests(BaseHTTPRequestHandler):
         (resource, id) = self.parse_url(self.path)
 
         # Initialize new animal
-        new_animal = None
-        new_employee=None
-        new_customer=None
+        new_entry = None
+        
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
-            new_animal = create_animal(post_body)
+            new_entry = create_animal(post_body)
 
         if resource =="employees":
-            new_employee=create_employee(post_body)
+            new_entry =create_employee(post_body)
 
         if resource=="customers":
-            new_customer=create_customer(post_body)
+            new_entry = create_customer(post_body)
         # Encode the new animal and send in response
-        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_entry}".encode())
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
+    
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+    # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+            
+        if resource == "customers":
+            update_customer(id, post_body)
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
     # Set a 204 response code
