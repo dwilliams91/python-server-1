c80232547f3647ed63251ea96600e4389018aa31
Your Github User Name <david.lav.williams@gmail.com>
2021-01-13 09:18:10 -0600

    chapter 7 is done but not tested

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .animals import Animal
+from .customer import Customers
+from .employee import Employee
+from .location import Location
---
+++ models/employee.py
@@ -0,0 +1,8 @@
+class Employee:
+
+    def __init__(self, id, name, animalId, locationId):
+        self.id = id
+        self.name = name
+        self.animalId=animalId
+        self.locationId=locationId
+
