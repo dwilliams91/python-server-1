aadb7b1b7b6d13f9a767b11bbc758ae696cbe07e
Your Github User Name <david.lav.williams@gmail.com>
2021-01-14 14:39:47 -0600

    customers sql for chapter 9 is working

--- customers/request.py
+++ customers/request.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Customers
+
 CUSTOMERS= [
     {
       "id": 1,
@@ -33,17 +37,75 @@ CUSTOMERS= [
 
 
 
+# def get_all_customers():
+#     with
+
 def get_all_customers():
-    return CUSTOMERS
+    # Open a connection to the database
+    with sqlite3.connect("./kennels.db") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.email,
+            c.password,
+            c.address
+        FROM customer c
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            customer = Customers(row['id'], row['name'], row['email'], row['password'], row['address'])
+
+            customers.append(customer.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(customers)
+
 
 def get_single_customer(id):
-    requested_customer=None
+    with sqlite3.connect("./kennels.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    for customer in CUSTOMERS:
-        if customer["id"]==id:
-            requested_customer=customer
-    
-    return requested_customer
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.email,
+            c.password,
+            c.address
+        FROM customer c
+        WHERE c.id=?
+        """, ( id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        customer = Customers(data['id'], data['name'], data['email'],
+                            data['password'], data['address'])
+
+    return json.dumps(customer.__dict__)
 
 def create_customer(customer):
     # Get the id value of the last animal in the list
--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,8 @@
 class Customers:
 
-    def __init__(self, id, name, email, password):
+    def __init__(self, id, name, email, password, address):
         self.id = id
         self.name = name
         self.email=email
         self.password=password
+        self.address= address
