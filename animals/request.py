3bb0d59005f11fe3bd03a03febf1aa1bf28d5822
Your Github User Name <david.lav.williams@gmail.com>
2021-01-19 11:58:12 -0600

    chatper 14 works

--- animals/request.py
+++ animals/request.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 from models import Animal
-
+from models import Location
 
 ANIMALS = [
     {
@@ -123,8 +123,12 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
+        FROM Animal a
+            JOIN Location l
+            ON l.id = a.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -136,14 +140,17 @@ def get_all_animals():
         # Iterate list of data returned from database
         for row in dataset:
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+    # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                    row['location_id'], row['customer_id'])
 
+    # Create a Location instance from the current row
+            location = Location(row['location_id'],row['location_address'], row['location_name'])
+
+    # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+
+    # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
 
     # Use `json` package to properly serialize list as JSON
